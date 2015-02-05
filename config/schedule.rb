fe90095226534762a75d77b99bded1cacbfec1f8
Atom <fromatom@gmail.com>
2015-02-05 17:48:11 +0900

    換気終了時間も教える

--- config/schedule.rb
+++ config/schedule.rb
@@ -6,7 +6,17 @@ every 1.day, :at => '12:20 pm' do
   command "cd #{Whenever.path} && bundle exec ruby docomo-announce.rb"
 end
 
+# 昼の換気終わり
+every 1.day, :at => '12:20 pm' do
+  command "cd #{Whenever.path} && bundle exec ruby docomo-announce.rb end"
+end
+
 # 夕方の換気
 every 1.day, :at => '5:00 pm' do
   command "cd #{Whenever.path} && bundle exec ruby docomo-announce.rb"
+end
+
+# 夕方の換気
+every 1.day, :at => '5:10 pm' do
+  command "cd #{Whenever.path} && bundle exec ruby docomo-announce.rb end"
 end
--- docomo-announce.rb
+++ docomo-announce.rb
@@ -7,8 +7,15 @@ API_KEY = 'YOUR_API_KEY'
 endpoint = URI.parse('https://api.apigw.smt.docomo.ne.jp/voiceText/v1/textToSpeech')
 endpoint.query = 'APIKEY=' + API_KEY
 
+time_param =  ARGV[0]
+if (time_param == 'end')
+  text = '換気終了の時間です。ドアと窓を閉めて、エアコンと加湿器と空気清浄機をつけましょう。'
+else
+  text = '本日も研究活動、お疲れ様です。そろそろ換気の時間です。窓とドアをあけて10分間の換気をしましょう。'
+end
+
 request_body = {
-  'text'=> '本日も研究活動、お疲れ様です。そろそろ換気の時間です。窓とドアをあけて10分間の換気をしましょう。',
+  'text'=> text,
   'speaker'=> 'hikari'
 }
 
